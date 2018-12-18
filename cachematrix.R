f0da1d58bc89a2387a5652200c7de2ab3ed108d1
henrik1348 <45802105+henrik1348@users.noreply.github.com>
2018-12-18 22:48:02 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix function ultimately returns a list of the inverse. 
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(x = matrix()) { #Default of argument x is an empty matrix.
+    inv = NULL                              #Creates inv, an empty object to be used later.    
+    set = function(y) {                     # function with argument y 
+        x <<-y                              # y is assigned to x, the matrix in parent eniviroment of makeCacheMatrix. 
+        inv <<- NULL                        # NULL is assigned to inv, also in parent enviroment.
+    }
+    get = function() x                      # x is not defined within function get(), therefore R retrive it from parent enivorment.
+    setinver = function(inverse)            # setter function for the inverse.
+        inv <<- inverse                     # inv is in parent enivorment.
+    getinver = function() inv               # R retrive the value of inv from parent enivorment.
+    list(set = set, 
+         get = get, 
+         setinver = setinver, 
+         getinver = getinver)               # Finally, a list is returned.
+}
 
-makeCacheMatrix <- function(x = matrix()) {
+## cacheSolve computes the inverse of the list that is returned from makeCacheMAtrix.
+## If the inverse has been computed already, then this function will get the inverse from the cache. 
+## This speed things up.
 
+
+cacheSolve <- function(x, ...) {           # argument x refers to the list makeCacheMatrix returns.
+    inv = x$getinv()                       # gets the inverse from x. 
+    
+    if (!is.null(inv)){                    # If the result is NULL..
+        message("getting cached data")
+        return(inv)                        # ...then return the value inv to parent enviroment.
+    }
+    
+    data = x$get()                         # If !is.null is FALSE, the function get x.  
+    inv = solve(data, ...)                 # And calucalte the inverse.
+    x$setinv(inv)                          #     
+    return(inv)                            # Returns the inverse.
 }
 
 
-## Write a short comment describing this function
+# Test
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+myMatrix_object <- makeCacheMatrix(m1)
+
+cacheSolve(myMatrix_object)
+
+#       [,1] [,2]
+# [1,]    6    8
+# [2,]    2    4
